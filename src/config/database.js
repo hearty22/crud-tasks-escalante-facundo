b7ee77f9411a20ebdc3d062ce1339bddc5f2e2dc
hearty_dev <facudnoesclante@gmail.com>
2025-08-07 09:51:16 -0300

    carpeta src: archivos para la conexion con la base de datos hecha

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT
+
+    }
+);
