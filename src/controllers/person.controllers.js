0c07106c515d78e36ec4e77598f7941c2ec5b49f
hearty_dev <facudnoesclante@gmail.com>
2025-08-19 11:06:44 -0300

    feat: controladores faltantes del modelo person

--- src/controllers/person.controllers.js
+++ src/controllers/person.controllers.js
@@ -1,5 +1,5 @@
 import { personModel } from "../models/person.model.js";
-import { Op } from "sequelize";
+import { json, Op } from "sequelize";
 import { userModel } from "../models/user.model.js";
 import { taskModel } from "../models/task.model.js";
 
@@ -42,11 +42,53 @@ export const createPerson = async (req, res)=>{
         if(!userExist){return res.status(400).json({message:"la persona debe de estar asociada a un usuario y dicho usuario debe de existir"})}
         const newPerson = new personModel({firstname, lastname, user_id});
         await newPerson.save();
-        res.status(200).json(newPerson);
+        res.status(201).json(newPerson);
     } catch (error) {
         res.status(500).json({error:"error al crear la persona"}
         )
     }
 }
 //PUT
+export const updatePerson = async (req, res)=>{
+    try {
+        const {firstname, lastname, user_id} = req.body;
+        const person = await personModel.findByPk(req.params.id);
+        if(!person){
+            return res.status(404).json({message:"la persona que desea editar no existe"});
+        }
+        if(!firstname||!lastname||!user_id){
+            return res.status(400).json({message:"campos obligatorios no rellenados: firstname, lastname, user_name"});
+        }
+        if(typeof user_id !== "number"){
+            return res.status(400).json({message:"el campo user_id debe de ser un entero"})
+        };
+          const userExist = await userModel.findByPk(user_id);
+
+        if(!userExist){
+            return res.status(400).json({message:"el user que desea asociar no existe"});
+        }
+        
+        await person.update({firstname, lastname, user_id});
+        console.log("aca pasa")
+        
+        res.status(200).json(person);
+
+    } catch (error) {
+        res.status(500).json({error: "error al actualizar a la persona"});
+    }
+};
 //DEL
+export const delPerson = async (req, res)=>{
+    try {
+        const person = await personModel.findByPk(req.params.id);
+        if(!person){
+            return res.status(404).json({message:"la persona que desea eliminar no existe"});
+        };
+        const {firstname, lastname, user_id} = person;
+        person.destroy();
+        res.status(200).json({message:`Persona eliminada con exito: ${firstname}, ${lastname}`})
+
+    } catch (error) {
+        res.status(500).json({error:"error al eliminar la persona"})
+    }
+}
